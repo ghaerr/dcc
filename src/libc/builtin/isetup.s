5abd338da3ae9548e287c228214022d2ec91d30a
Greg Haerr <greg@censoft.com>
2026-02-16 14:13:46 -0700

    Add _sbrk, _fmemalloc, unlink to libc

--- src/libc/builtin/isetup.s
+++ src/libc/builtin/isetup.s
@@ -6,6 +6,9 @@
 ; 26 Dec 24 Added C startup code for argc/argv and exit return
 ; 14 Feb 26 Ported C86 syscall.s to C88 isetup.s
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;
+include "syselks.h"
+;
         CSEG                    ; first text seg
         nop                     ; prevent text having address 0 for SIG_DFL,SIG_IGN
         nop
@@ -22,7 +25,7 @@ __stacklow_:dw      0
         ;.comm   ___program_filename,2
 
         CSEG
-        PUBLIC  _CSETUP,main_,exit_,_exit_
+        PUBLIC  _CSETUP,main_
 _CSETUP:                        ; C program entry point
         mov     ax,sp
         sub     ax,dx           ; DX is stack size
@@ -75,35 +78,50 @@ L2:
         PUBLIC  __exit
 exit_:                          ; C exit temp comes here
 _exit_:                         ; _exit
-        mov     ax,1
+        mov     ax,SYS_exit
         jmp     callsys
 
         PUBLIC  read_
 read_:
-        mov     ax,3
+        mov     ax,SYS_read
         jmp     callsys
 
         PUBLIC  write_
 write_:
-        mov     ax,4
+        mov     ax,SYS_write
         jmp     callsys
 
         PUBLIC  open_
 open_:
-        mov     ax,5
+        mov     ax,SYS_open
         jmp     callsys
 
         PUBLIC  close_
 close_:
-        mov     ax,6
+        mov     ax,SYS_close
         jmp     callsys
 
         PUBLIC  _lseek_
 _lseek_:
-        mov     ax,19
+        mov     ax,SYS_lseek
         jmp     callsys
 
         PUBLIC  ioctl_
 ioctl_:
-        mov     ax,54
+        mov     ax,SYS_ioctl
+        jmp     callsys
+
+        PUBLIC  _sbrk_
+_sbrk_:
+        mov     ax,SYS_sbrk
+        jmp     callsys
+
+        PUBLIC  _fmemalloc_
+_fmemalloc_:
+        mov     ax,SYS_fmemalloc
+        jmp     callsys
+
+        PUBLIC  unlink_
+unlink_:
+        mov     ax,SYS_unlink
         jmp     callsys
