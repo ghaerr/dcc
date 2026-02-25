ebfabea710aede3949d88d0f85bee35c9b8986ec
Greg Haerr <greg@censoft.com>
2026-02-25 09:17:49 -0700

    Add time() libc routine

---
+++ src/libc/system/time.c
@@ -0,0 +1,16 @@
+#include <time.h>
+
+time_t
+time(time_t *where)
+{
+   struct timeval rv;
+
+   if (gettimeofday(&rv, (void*)0) < 0)
+      return (time_t)-1;
+
+   if(where)
+      *where = rv.tv_sec;
+
+   return rv.tv_sec;
+}
+
